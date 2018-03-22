3ff550b16e921aa3fabf0ef23a675aa3e851229e
silentsvv <silentsvv@163.com>
2018-03-23 00:11:27 +0800

    init

---
+++ src/main/java/com/imooc/ajaxserver/AjaxserverApplication.java
@@ -0,0 +1,12 @@
+package com.imooc.ajaxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxserverApplication.class, args);
+	}
+}
